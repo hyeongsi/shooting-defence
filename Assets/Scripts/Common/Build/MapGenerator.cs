8d5de1febcd10191b138382ced02e57cdcce2c43
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-09-07 02:08:16 +0900

    블럭 생성 프리뷰 추가
    1. 블럭 생성 프리뷰 추가
    - 프리뷰 블럭을 생성하고, 조건에 맞다면, 실제 블럭으로 변경한다.
    
    - 초록, 빨강 메테리얼을 만들어 사용했으며, 생성 유무를 표시해준다.
    
    - 생성, 삭제 시 태그를 바꿔서 기존 블럭과 프리뷰를 구분했다.

--- Assets/Scripts/Common/Build/MapGenerator.cs
+++ Assets/Scripts/Common/Build/MapGenerator.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     [SerializeField]
-    private GameObject childBlockGameObject = null;
+    private GameObject parentBlockGameObject = null;
     [SerializeField]
-    private GameObject childBarricadeGameObject = null;
+    private GameObject parentBarricadeGameObject = null;
     [SerializeField]
-    private GameObject childTurretGameObject = null;
+    private GameObject parentTurretGameObject = null;
+    [SerializeField]
+    private GameObject parentTransparentObject = null;
+    private GameObject transparentObject = null;
+
+    public Material[] blockMaterial;
 
     private Ray ray;
     private RaycastHit hit;
     private int rayermask;
 
-    public Transform[] tilePrefab;
     private uint selectPrefab = 0;
     private float currentRotationAngle = 0;
     private const float rotationAngle = 90.0f;
 
+    private bool isBuildMode = true;
     private bool isEditMode = true;
+
+    public Transform[] tilePrefab;
 
     public bool IsEditMode
     {
@@ -104,7 +111,32 @@ public class MapGenerator : MonoBehaviour
         return pointVector;
     }
 
-    public void GenerateBlock(RaycastHit hit, Block block)         // 블럭 생성
+    public void GenerateBlock()         // 블럭 생성
+    {
+        if (transparentObject == null)
+            return;
+
+        switch (transparentObject.GetComponent<Block>().BlockType)     // 블록 유형에 따라 부모 오브젝트 설정, (정리)
+        {
+            case BlockType.BLOCK:
+                transparentObject.transform.parent = parentBlockGameObject.transform;
+                break;
+            case BlockType.BARRICADE:
+                transparentObject.transform.parent = parentTurretGameObject.transform;
+                break;
+            case BlockType.TURRET:
+                transparentObject.transform.parent = parentBarricadeGameObject.transform;
+                break;
+            default:
+                transparentObject.transform.parent = parentBlockGameObject.transform;
+                break;
+        }
+
+        transparentObject.gameObject.layer = (int)LayerNumbering.BLOCK;
+        transparentObject.transform.GetChild(0).GetComponent<Renderer>().material = blockMaterial[(int)TransparentMaterialColor.YELLOW_GRID_COLOR_MATERIAL];
+        transparentObject = null;
+    }
+    public void GenerateTransparentBlock(RaycastHit hit, Block block)         // 블럭 생성
     {
         Vector3 createDirection;
         Vector3 createBlockPosition;
@@ -119,59 +151,92 @@ public class MapGenerator : MonoBehaviour
             createBlockPosition.y = hit.transform.position.y + createDirection.y * newBlockSize.y;
             createBlockPosition.z = hit.transform.position.z + createDirection.z * newBlockSize.z;
 
-            if (FindBlocks(createBlockPosition, newBlockSize)) // 설치 위치에 블럭이 이미 존재하면 생성 X
-                return;
-
             Transform newBlock = Instantiate(tilePrefab[selectPrefab], createBlockPosition, Quaternion.Euler(Vector3.zero)) as Transform;
-            switch(block.BlockType)     // 블록 유형에 따라 부모 오브젝트 설정, (정리)
+            transparentObject = newBlock.gameObject;
+            transparentObject.transform.parent = parentTransparentObject.transform;
+
+            if (FindBlocks(createBlockPosition, newBlockSize)) // 설치 위치에 블럭이 이미 존재하면 붉은 오브젝트 출력
             {
-                case BlockType.BLOCK:
-                    newBlock.parent = childBlockGameObject.transform;
-                    break;
-                case BlockType.BARRICADE:
-                    newBlock.parent = childTurretGameObject.transform;
-                    break;
-                case BlockType.TURRET:
-                    newBlock.parent = childBarricadeGameObject.transform;
-                    break;
-                default:
-                    newBlock.parent = childBlockGameObject.transform;
-                    return;
+                newBlock.GetChild(0).GetComponent<Renderer>().material = blockMaterial[(int)TransparentMaterialColor.GREEN_COLOR_MATERIAL];
+            }
+            else
+            {
+                newBlock.GetChild(0).GetComponent<Renderer>().material = blockMaterial[(int)TransparentMaterialColor.RED_COLOR_MATERIAL];
             }
 
+            newBlock.gameObject.layer = (int)LayerNumbering.DEFAULT;
             newBlock.localRotation = Quaternion.Euler(0, currentRotationAngle, 0);
         }
     }
+
+    public void SetTransparentBlockTransform(RaycastHit hit, Block block)
+    {
+        Vector3 createDirection;
+        Vector3 createBlockPosition;
+
+        if (selectPrefab >= 0 && selectPrefab < tilePrefab.Length)    // 없는 블럭은 생성하지 못하도록
+        {
+            createDirection = FindDirection(hit.point - hit.transform.position, block.BlockSize);
+            Vector3 newBlockSize = tilePrefab[selectPrefab].GetComponent<Block>().BlockSize;
+
+            // 생성될 블럭 위치 계산
+            createBlockPosition.x = hit.transform.position.x + createDirection.x * newBlockSize.x;
+            createBlockPosition.y = hit.transform.position.y + createDirection.y * newBlockSize.y;
+            createBlockPosition.z = hit.transform.position.z + createDirection.z * newBlockSize.z;
+
+            if (transparentObject.transform.position != createBlockPosition)
+                transparentObject.transform.position = createBlockPosition;
+
+            transparentObject.transform.localRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+        }
+    }
+
     private void Start()
     {
         rayermask = 1 << LayerMask.NameToLayer("Block");
     }
 
     private void Update()
-    {  
-        if(Input.GetKeyDown(KeyCode.R))     // 블럭 회전
-        {
+    {
+        if (!isBuildMode)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.R))     // 블럭 회전
             currentRotationAngle += rotationAngle;
-        }
 
-        if (Input.GetMouseButtonUp(0))      // 좌클릭, 블럭 생성
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f, rayermask))
         {
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, rayermask))
-            {
-                GenerateBlock(hit, hit.transform.GetComponent<Block>());
-            }
-        }
-
-        if (Input.GetMouseButtonUp(1))      // 우클릭, 블럭 삭제
-        {
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, rayermask))
+            if (Input.GetMouseButtonUp(0))           // 좌클릭, 블럭 생성
+                GenerateBlock();
+            else if (Input.GetMouseButtonUp(1))      // 우클릭, 블럭 삭제
             {
                 DestroyImmediate(hit.transform.gameObject);
             }
+            else
+            {
+                if (transparentObject == null)
+                    GenerateTransparentBlock(hit, hit.transform.GetComponent<Block>());
+                else
+                    SetTransparentBlockTransform(hit, hit.transform.GetComponent<Block>());
+            }
+        }else
+        {
+            if (transparentObject != null)
+                DestroyImmediate(transparentObject);
         }
+    }
+
+    private enum TransparentMaterialColor
+    {
+        GREEN_COLOR_MATERIAL = 0,
+        RED_COLOR_MATERIAL = 1,
+        YELLOW_GRID_COLOR_MATERIAL = 2,
+    }
+
+    private enum LayerNumbering
+    {
+        DEFAULT = 0,
+        BLOCK = 8,
     }
 }
