087cea80e3b6e7d812f98462492443cd91f0e18c
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-09-13 16:51:56 +0900

    데이터 저장 테스트

--- Assets/Scripts/Common/Manager/MapManager.cs
+++ Assets/Scripts/Common/Manager/MapManager.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEditor;
 
 // 맵 불러오기, 저장, 현재 맵 정보
 public class MapManager : MonoBehaviour
 {
-    // 건축물 타입
-    // 맵 크기 (x,y)
-    // 맵 크기에 따른 건축물 배열 (x,y,z)
-    // 
+    BinaryFormatter bf;
+    FileStream file;
 
     #region Singleton
     private static MapManager instance = null;
@@ -46,17 +47,66 @@ public class MapManager : MonoBehaviour
     #endregion
 
     #region MapLoader
+
+    // 파일 경로 지정
+    public void FileSelect()
+    {
+        string path;
+
+        path = EditorUtility.OpenFilePanel("Overwrite with png", "", "dat");
+
+        Debug.Log(path);
+    }
+
     // 맵 저장
     public void SaveMap()
     {
+        FileSelect();
 
+        string path = Application.dataPath + "/mapData";
+        string dataPath = "/test.dat";
+
+        Debug.Log(path);
+
+        if(!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        file = new FileStream(path + dataPath, FileMode.OpenOrCreate);
+
+        int a = 20;
+
+        bf.Serialize(file, a);
+        file.Close();
     }
 
     // 맵 불러오기
     public void LoadMap()
     {
+        FileSelect();
 
+        string path = Application.dataPath + "/mapData";
+        string dataPath = "/test.dat";
+
+        if (!File.Exists(path + dataPath))
+        {
+            Debug.Log("불러올 파일이 존재하지 않습니다.");
+            return;
+        }
+
+        file = File.Open(path + dataPath, FileMode.Open);
+
+        int a = (int)bf.Deserialize(file);
+
+        Debug.Log(a);
+
+        file.Close();
     }
     #endregion
 
+    private void Start()
+    {
+        bf = new BinaryFormatter();
+    }
 }
