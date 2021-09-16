7a01e39066ac2a14d5fc2e60df07904100ba99bb
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-09-16 23:23:32 +0900

    블럭 선택 오류 수정
    1. 블럭 선택 오류 수정
    - 오브젝트가 비활성화 되어 있어 스크립트의 start 부분이 실행되지 않던 문제 수정,
    
    - 시작하면, 초기화 부분 실행 후, 해당 오브젝트 false 되도록 변경,
    
    - 초기화 부분 실행하고, 해당 오브젝트 비활성화 하도록 스크립트 순서 변경

--- Assets/Scripts/MapEdittor/UI/Build UI/MapEditUIController.cs
+++ Assets/Scripts/MapEdittor/UI/Build UI/MapEditUIController.cs
@@ -19,6 +19,14 @@ public class MapEditUIController : MonoBehaviour
             transform.Find("Build Menu UI")?.GetChild(0).gameObject.SetActive(false);
     }
 
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
