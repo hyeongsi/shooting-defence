afbbf4a26647d228b81748e39b2e1d0bc88bb4cf
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-10-08 23:47:30 +0900

    singleton 코드 생성
    1. singleton 코드 생성
    - 해당 코드로 다른 싱글톤 코드들 코드 정리 수행,
    
    - 기존의 매니저 코드의 프리팹 들은 리소스 매니저를 생성해서 Resource.Load()로 불러서 사용하도록 변경할 예정,
    
    - 로딩 시 다음 씬 초기화 문제는 스트리밍 레벨 <-- 기능을 구현해서 초기화 할 예정,
    
    - 스트리밍 레벨 : 메모리 사용량을 줄이고 심리스, 로딩화면 없는 월드를 만들기 위해 플레이 도중 레벨을 비동기 로드/언로드 하는 기법

---
+++ Assets/Scripts/Common/Manager/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public T Instance
+    {
+        get
+        {
+            if (instance != null)
+            {
+                return instance;
+            }
+
+            instance = (T)FindObjectOfType(typeof(T));  // T타입 오브젝트 있는지 찾기 
+
+            if(instance == null)
+            {
+                GameObject singleton = new GameObject();
+                instance = singleton.AddComponent<T>();
+                singleton.name = "(singleton)" + typeof(T).ToString();
+
+                DontDestroyOnLoad(singleton);
+            }
+
+            return instance;
+        }
+    }
+}
