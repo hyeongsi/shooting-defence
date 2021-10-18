ab95b11871d05415d27c59e72db256b37ca781e6
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-10-18 23:56:13 +0900

    맵 생성 ui, 블록 선택 ui
    1. 맵 생성 ui
    - 팝업창을 통해 맵의 크기를 받아 맵을 생성할 수 있도록 구현
    
    2. 블록 선택 ui
    - 블록 버튼을 통해 스크롤뷰를 토대로 만들어진 블록 선택창이 출력되도록 구현

---
+++ Assets/Scripts/MapEditor/UI/UI_Scene_MapEditor.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Scene_MapEditor : UI_Scene
+{
+    private BlockManager.BlockName selectBlockIndex = BlockManager.BlockName.Block1_Gray;
+    private InputField xInputField;
+    private InputField yInputField;
+
+    private void Start()
+    {
+        Init();
+        UIManager.Instance.ShowPopupUI(UIManager.MainMenuPopUpUI.SelectStage_Canvas.ToString());
+    }
+
+    public void SwitchActiveCavnas(Canvas canvas)
+    {
+        if (canvas == null)
+            return;
+
+        if(canvas.gameObject.activeSelf == true)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+        else
+        {
+            canvas.gameObject.SetActive(true);
+        }
+    }
+
+    public void SetInputFieldX(InputField xInputField)
+    {
+        this.xInputField = xInputField;
+    }
+
+    public void SetInputFieldY(InputField yInputField)
+    {
+        this.yInputField = yInputField;
+    }
+
+    public void GenerateMap()
+    {
+        const string GENERATE_MAP_PARENT_NAME = "Generated Map";
+
+        int xValue = Int32.Parse(xInputField.text);
+        int yValue = Int32.Parse(yInputField.text);
+
+        if (xValue < 0)
+            xValue = 0;
+
+        if (yValue < 0)
+            yValue = 0;
+
+        GameObject generateBlockGameObject = BlockManager.Instance.GetBlock(selectBlockIndex);
+        GameObject generateMapParent = GameObject.Find(GENERATE_MAP_PARENT_NAME);
+
+        if (generateBlockGameObject == null)
+            return;
+
+        if (generateMapParent != null)
+        {
+            DestroyImmediate(generateMapParent);
+        }
+
+        generateMapParent = new GameObject(GENERATE_MAP_PARENT_NAME);
+
+        for (int x = 0; x < xValue; x++)
+        {
+            for (int y = 0; y < yValue; y++)
+            {
+                Vector3 tilePosition = new Vector3(x, 0, -y);
+                Transform newTile = Instantiate(generateBlockGameObject.transform, tilePosition, Quaternion.identity);
+                newTile.parent = generateMapParent.transform;
+            }
+        }
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
