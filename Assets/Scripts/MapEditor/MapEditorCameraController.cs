4b2d90642f987042f5cd1b5e4c4aaebb52c7a3cc
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-10-31 17:23:16 +0900

    맵 에디터 카메라 이동 구현
    1. 맵 에디터 카메라 이동 구현
    - 메인 카메라에 스크립트 붙이고 update로 구현
    
    - space : 위치 초기화, 방향키,wsad : x, z 축 이동, 마우스 스크롤 : 줌인, 줌아웃

---
+++ Assets/Scripts/MapEditor/MapEditorCameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEditorCameraController : MonoBehaviour
+{
+    private Vector3 originPos;
+    private float moveSpeed = 4.0f;
+    private float zoomSpeed = 4.0f;
+    private float horizontal = 0.0f;
+    private float vertical = 0.0f;
+
+    private void Start()
+    {
+        originPos = transform.position;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = originPos;
+        }
+
+        MoveCamera();
+        ZoomCamera();
+    }
+
+    private void MoveCamera()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        transform.position += (transform.right * horizontal + transform.up * vertical).normalized * moveSpeed * Time.deltaTime;
+    }
+
+    private void ZoomCamera()
+    {
+        float mouseScrollDeltaY = Input.mouseScrollDelta.y;
+
+        transform.position += transform.forward * mouseScrollDeltaY * zoomSpeed * Time.deltaTime;
+    }
+
+}
