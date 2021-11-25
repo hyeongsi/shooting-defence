6227d467d4840d084b04a6077b6a4acf0f2958d2
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-11-25 16:51:46 +0900

    캐릭터 소환
    캐릭터 소환

--- Assets/Scripts/Common/InGameMapController.cs
+++ Assets/Scripts/Common/InGameMapController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class InGameMapController : MonoBehaviour
 {
+    //public GameObject player;
+    public Transform playerPrefab;
+    public CinemachineVirtualCamera cvm;
     MapEditorController.CustomTileMap customTileMap = new MapEditorController.CustomTileMap();
 
     void Start()
@@ -12,8 +16,6 @@ public class InGameMapController : MonoBehaviour
         BlockManager.Instance.LoadAll();
         EnemyManager.Instance.LoadAll();
         ObjManager.Instance.LoadAll();
-
-        LoadCustomMap("test4");
     }
 
     public void LoadCustomMap(string mapName)
@@ -21,7 +23,15 @@ public class InGameMapController : MonoBehaviour
         TextAsset text = Resources.Load(mapName) as TextAsset;
         customTileMap = JsonUtility.FromJson<MapEditorController.CustomTileMap>(text.ToString());
 
+        // 체크포인트 등록, (적 이동 경로)
+        // 적 스폰지점
         customTileMap.CreateCustomMap();
+
+        Transform newPlayer = Instantiate(playerPrefab, customTileMap.spawnPosition.placeGameTransform, Quaternion.identity);
+
+        cvm.Follow = newPlayer;
+        cvm.LookAt = newPlayer;
+        //player.transform.position = customTileMap.spawnPosition.placeGameTransform;
     }
 
 }
